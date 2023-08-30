57e079fe54a9bb06db0e62d9c62ba630043592d8
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2023-08-30 20:17:29 -0300

    Added corotine to ItemHolderDetector to check objs distances

--- Assets/Scripts/ItemHolderDetector.cs
+++ Assets/Scripts/ItemHolderDetector.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,46 +9,111 @@ public class ItemHolderDetector : MonoBehaviour {
 
     [SerializeField] int playerIndex;
     ItemHolder playerItemHolder;
-    List<ItemHolder> selectedItemHolders;
+    List<ItemHolder> possibleHolders;
+    ItemHolder selectedHolder;
+    float currDistToHolder;
+    bool shouldCheckDistance = false;
+    float checkRefreshTime = 0.1f;
 
     void Awake() {
-        selectedItemHolders = new List<ItemHolder>();
+        possibleHolders = new List<ItemHolder>();
         playerItemHolder = GetComponent<ItemHolder>();
     }
 
     void Start() {
         EventManager.current.onFirePerformed += onInteract;
+        EventManager.current.onMovePerformed += startCheck;
+        EventManager.current.onMoveCanceled += stopCheck;
     }
 
     void OnDestroy() {
+        StopAllCoroutines();
         EventManager.current.onFirePerformed -= onInteract;
+        EventManager.current.onMovePerformed -= startCheck;
+        EventManager.current.onMoveCanceled -= stopCheck;
     }
 
     void OnTriggerEnter(Collider other) {
         ItemHolder holder = other.GetComponent<ItemHolder>();
-        if (holder != null) {
-            if (holder.gameObject.tag != "Player" || !holder.isEmpty()) {
-                selectedItemHolders.Add(holder);
-                holder.highlightObject();
-            }
+        if (holder == null) return;
+
+        possibleHolders.Add(holder);
+        if (selectedHolder == null) {
+            currDistToHolder = Vector3.Distance(transform.position, holder.transform.position);
+            selectHolder(holder);
+        }
+        else {
+            selectIfCloser(holder);
         }
     }
 
     void OnTriggerExit(Collider other) {
         ItemHolder holder = other.GetComponent<ItemHolder>();
-        if (holder != null) {
-            selectedItemHolders.Remove(holder);
-            holder.unhighlightObject();
+        if (holder == null) return;
+
+        possibleHolders.Remove(holder);
+        if (selectedHolder == holder) {
+            selectHolder(possibleHolders.FirstOrDefault());
         }
     }
 
-    public void onInteract(int playerIndex) {
-        if (playerIndex == this.playerIndex && selectedItemHolders.Any()) {
+    void startCheck(int playerIndex) {
+        if (playerIndex != this.playerIndex) return;
+        setDistanceCheck(true);
+    }
+
+    void stopCheck(int playerIndex) {
+        if (playerIndex != this.playerIndex) return;
+        setDistanceCheck(false);
+    }
+
+    void setDistanceCheck(bool value) {
+        if (shouldCheckDistance == value) return;
+        shouldCheckDistance = value;
+        if (value) {
+            StartCoroutine(checkHoldersDistance());
+        }
+        else {
+            StopCoroutine(checkHoldersDistance());
+        }
+    }
+
+    IEnumerator checkHoldersDistance() {
+        while (shouldCheckDistance) {
+            foreach (var holder in possibleHolders) {
+                selectIfCloser(holder);
+            }
+            yield return new WaitForSeconds(checkRefreshTime);
+        }
+    }
+
+    void selectHolder(ItemHolder holder) {
+        selectedHolder?.unhighlightObject();
+        selectedHolder = holder;
+        selectedHolder?.highlightObject();
+        if (selectedHolder == null) {
+            currDistToHolder = float.MaxValue;
+        }
+    }
+
+    void selectIfCloser(ItemHolder holder) {
+        float dist = Vector3.Distance(transform.position, holder.transform.position);
+        if (holder == selectedHolder) {
+            currDistToHolder = dist;
+        }
+        if (currDistToHolder > dist) {
+            currDistToHolder = dist;
+            selectHolder(holder);
+        }
+    }
+
+    void onInteract(int playerIndex) {
+        if (playerIndex == this.playerIndex && possibleHolders.Any()) {
             if (playerItemHolder.isEmpty()) {
-                playerItemHolder.placeItem(selectedItemHolders[0].takeOutItem());
+                playerItemHolder.placeItem(selectedHolder.takeOutItem());
             }
             else {
-                selectedItemHolders[0].placeItem(playerItemHolder.takeOutItem());
+                selectedHolder.placeItem(playerItemHolder.takeOutItem());
             }
         }
     }
